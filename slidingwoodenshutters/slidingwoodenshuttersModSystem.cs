9d8e6f609ef648522646b2f816e48ecf5d383fda
Antoine O'Donnell <125301788+AntoineOD@users.noreply.github.com>
2025-07-16 10:21:48 -0400

    Upload of the mod to git

---
+++ slidingwoodenshutters/slidingwoodenshuttersModSystem.cs
@@ -0,0 +1,32 @@
+﻿using slidingwoodenshutters.Blocks;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace slidingwoodenshutters
+{
+    public class slidingwoodenshuttersModSystem : ModSystem
+    {
+
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod: " + api.Side);
+            api.RegisterBlockClass(Mod.Info.ModID + ".slidingwoodenshutters", typeof(BlockWoodenShutters));
+            api.RegisterBlockEntityClass("BEWoodenShutters", typeof(BEWoodenShutters));
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("slidingwoodenshutters:hello"));
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("slidingwoodenshutters:hello"));
+        }
+
+    }
+}
